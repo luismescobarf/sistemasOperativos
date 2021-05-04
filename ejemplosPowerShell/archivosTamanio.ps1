42fa003394b05b2659b15c6aaf404152e9d4a4fb
luismescobarf <luismescobarf@gmail.com>
2021-05-04 12:33:40 -0500

    Estructura de carpetas y ejemplo de clasificación de archivos por tamaño

---
+++ ejemplosPowerShell/archivosTamanio.ps1
@@ -0,0 +1,7 @@
+# Get all files sorted by size.
+#$info = Get-ChildItem -Path '/Users/luismiguelescobarfalcon/Dropbox/UniLibre/SistemasOperativos/' -Recurse -Force -File | Select-Object -Property FullName,@{Name='SizeGB';Expression={$_.Length / 1GB}},@{Name='SizeMB';Expression={$_.Length / 1MB}},@{Name='SizeKB';Expression={$_.Length / 1KB}} | Sort-Object { $_.SizeKB } -Descending
+$info = Get-ChildItem -Path '/Users/luismiguelescobarfalcon/Downloads/' -Recurse -Force -File | Select-Object -Property FullName,@{Name='SizeGB';Expression={$_.Length / 1GB}},@{Name='SizeMB';Expression={$_.Length / 1MB}},@{Name='SizeKB';Expression={$_.Length / 1KB}} | Sort-Object { $_.SizeKB } -Descending
+Write-Host "Mayor Ordenamiento " $info[0].FullName
+Write-Host "Segundo Mayor " $info[1].FullName
+#Invoke-Item $info[0].FullName
+Invoke-Item $info[1].FullName
